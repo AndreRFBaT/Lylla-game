ab09eda3720a36fb3796342cecc1a8104e9a59e7
Andre R. F. Batista <andrerfbatista@gmail.com>
2026-09-22 14:26:13 -0300

    ENH: Remove blank spaces on script

--- GeneralNodes/HitBox/hit_box.gd
+++ GeneralNodes/HitBox/hit_box.gd
@@ -10,7 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
 
 func TakeDamege( hurt_box : HurtBox ) -> void:
 	print("=== [HITBOX RECEIVED GAMAGE] ===")
